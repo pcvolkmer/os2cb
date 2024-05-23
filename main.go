62e89c40377b68716dbb43172600751970c3c7c1
Paul-Christian Volkmer <volkmer_p@ukw.de>
2024-05-23 17:16:00 +0200

    Remove DB connection limit

--- main.go
+++ main.go
@@ -103,7 +103,6 @@ func main() {
 
 	if dbx, err := sql.Open("mysql", dbCfg.FormatDSN()); err == nil {
 		if err := dbx.Ping(); err == nil {
-			dbx.SetMaxOpenConns(25)
 			db = dbx
 			defer func(db *sql.DB) {
 				err := db.Close()
